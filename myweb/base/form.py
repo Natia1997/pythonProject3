8d79772e938075a14435ea32c041c4c47a12dbdd
Natia1997 <115346506+Natia1997@users.noreply.github.com>
2024-06-02 22:11:50 +0400

    Delete myweb/base directory

--- myweb/base/form.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Room, Meal
-
-class MealForm(ModelForm):
-    class Meta:
-        model = Meal
-        fields = ['name', 'description','picture']
